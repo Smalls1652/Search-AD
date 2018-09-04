38548ee71ea46b3047383e956203c7d05eeb2d0b
Timothy Small <tlsmall142@nashcc.edu>
2018-09-04 10:42:12 -0400

    Initial commit

---
+++ Search-ADUser.psd1
@@ -0,0 +1,11 @@
+@{
+    ModuleToProcess = 'Search-ADUser.psm1'
+    ModuleVersion = '1.0'
+    Author = 'Smalls'
+    Guid="648fe0ba-98ed-4a42-a250-a60bcea72a16"
+    CompanyName = 'Smalls'
+    Copyright = '(c) Tim Small. All rights reserved.'
+    Description = 'Search AD for users.'
+    FunctionsToExport = 'Search-ADUser'
+    CmdletsToExport = '*'
+    }    
---
+++ Search-ADUser.psm1
@@ -0,0 +1,80 @@
+function Search-ADUser {
+
+    [cmdletbinding()]
+    param(
+        [string]$FirstName,
+        [string]$LastName,
+        [string]$UserName,
+        [string]$Email,
+        [switch]$Strict
+    )
+
+    function New-UserDataObject {
+        param(
+            [Microsoft.ActiveDirectory.Management.ADUser]$UserData
+        )
+
+        $returnObj = New-Object -TypeName pscustomobject
+
+        $returnObj.PSObject.TypeNames.Insert(0, "Search.ActiveDirectory.Users")
+
+        $defaultOutput = "FirstName", "LastName", "UserName", "Email"
+        $defaultPropertSet = New-Object System.Management.Automation.PSPropertySet("DefaultDisplayPropertySet", [string[]]$defaultOutput)
+        $CustomOutput = [System.Management.Automation.PSMemberInfo[]]@($defaultPropertSet)
+
+        Add-Member -InputObject $returnObj -MemberType MemberSet -Name PSStandardMembers -Value $CustomOutput
+
+        Add-Member -InputObject $returnObj -MemberType NoteProperty -Name "FirstName" -Value $user.GivenName
+        Add-Member -InputObject $returnObj -MemberType NoteProperty -Name "LastName" -Value $user.Surname
+        Add-Member -InputObject $returnObj -MemberType NoteProperty -Name "UserName" -Value $user.CN
+        Add-Member -InputObject $returnObj -MemberType NoteProperty -Name "Email" -Value $user.EmailAddress 
+        Add-Member -InputObject $returnObj -MemberType NoteProperty -Name "LastLogon" -Value $user.LastLogonDate
+        Add-Member -InputObject $returnObj -MemberType NoteProperty -Name "DistinguishedName" -Value $user.DistinguishedName
+        Add-Member -InputObject $returnObj -MemberType NoteProperty -Name "SID" -Value $user.SID
+        Add-Member -InputObject $returnObj -MemberType NoteProperty -Name "Groups" -Value $user.MemberOf
+
+        return $returnObj
+    }
+
+    if ($Strict) {
+        $searchType = "-eq"
+    }
+    else {
+        $searchType = "-like"
+    }
+
+
+    $searchObjects = @()
+    if ($FirstName) {
+        $searchObjects += @{"GivenName" = $FirstName}
+    }
+    if ($LastName) {
+        $searchObjects += @{"Surname" = $LastName}
+    }
+    if ($UserName) {
+        $searchObjects += @{"CN" = $UserName}
+    }
+    if ($Email) {
+        $searchObjects += @{"EmailAddress" = $Email}
+    }
+
+    $searchString = ""
+    $i = 1
+    foreach ($obj in $searchObjects) {
+        $searchString += "$($obj.Keys) $($searchType) ""$($obj.Values)"""
+
+        if ($i -ne $searchObjects.Count) {
+            $searchString += " -and "
+        }
+        $i++
+    }
+
+    $adSearch = Get-ADUser -Filter $searchString -Properties *
+
+    foreach ($user in $adSearch) {
+
+        New-UserDataObject -UserDate $user
+
+    }
+
+}
